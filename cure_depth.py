e99e769a633ae6f2c10970192d4763ba77fe6ff5
Aviollet74 <arnaud.viollet@epfl.ch>
2025-01-07 16:30:36 +0100

    07.01.2025 rpi4

--- cure_depth.py
+++ cure_depth.py
@@ -18,7 +18,7 @@ root.geometry(f"{w_root}x{h_root}+{x_shift}+{y_shift}")                         
 cnv = tk.Canvas(root, bg="black", highlightthickness=0)
 cnv.pack(fill=tk.BOTH, expand=True)
 
-time_test=float(input("time tested"))
+# time_test=float(input("time tested"))
 black_image_tk  = display.convert_full_0("/home/alchemy/square.png", w_root, h_root, monitors)
 
 uv_pin = 27
