a311e2c3754c0136ee1af6c9262b306b837a4000
AViollet74 <arnaud.viollet@epfl.ch>
2025-03-19 09:34:59 +0100

    19.03 pc1

---
+++ function_motor2.py
@@ -0,0 +1,13 @@
+import board
+from time import sleep
+from time import monotonic
+from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
+import gpiod
+from time import time
+
+
+
+
+
+
