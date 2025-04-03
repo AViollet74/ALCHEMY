f2374bbd9b39772caaf5e88757c0e57ad7f20c0e
Aviollet74 <arnaud.viollet@epfl.ch>
2025-04-03 15:13:46 +0200

    03.04.2025

--- dogbone.py
+++ dogbone.py
@@ -101,8 +101,8 @@ else:
 
 ###################################################################################################################################
 #GUI creation with TkInter 
-for m in get_monitors():
-    print("INFO", str(m))
+# for m in get_monitors():
+#     print("INFO", str(m))
 
 monitors = get_monitors()
 monitor=display.name_selection('HDMI-2')                                                                # port for the LCD screen
@@ -158,9 +158,9 @@ for j in range(0,nb_layers, subset_imagetk):                                    
         start_time=time()
         percentage=(i+j)/nb_layers*100
         print(f"printing layer {i+j}_________________________________{percentage:.1f}% Complete")
-        motor2.move_dist_time_dir_dm(2,0.5, 1,1)
+        motor2.move_dist_time_dir_dm(2,0.4, 1,1)
         sleep(2)
-        motor2.move_dist_time_dir_dm(2-layer_thickness,0.5,-1,1)
+        motor2.move_dist_time_dir_dm(2-layer_thickness,0.4,-1,1)
         sleep(2)
         Z_table_pos+=layer_thickness
         layer_index+=1
@@ -172,7 +172,7 @@ for j in range(0,nb_layers, subset_imagetk):                                    
 
         ##  PARTICLES ACTUATION IN THE CONTAINER      
         if layer_index<=3:
-            cure_time =40                                                                               # 12 for commercial resin, 96 for custom resin 1, 
+            cure_time =50                                                                               # 12 for commercial resin, 96 for custom resin 1, 
         else:
             cure_time=20                                                                                # 2.8 for commercial resin, 25 for custom resin 1, 
         attract_time =500                                                                                # steady magnet time in seconds
