6b36f1657bfbb64e78be055f46fb0c0d8bd0aa68
Aviollet74 <arnaud.viollet@epfl.ch>
2025-05-05 16:16:42 +0200

    12133

--- functions/function_resin.py
+++ functions/function_resin.py
@@ -17,8 +17,8 @@ def resin_selection():
         exp_time=180
         exp_time_first=200 
     elif answer=="BA/HEMA 2":
-        exp_time=200
-        exp_time_first=220        
+        exp_time=240
+        exp_time_first=270        
     elif answer=="PEGDA/Water":
         exp_time=25
         exp_time_first=96        
